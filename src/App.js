d4c9fc0d41d1c1964417bb504e2674d4dca37b42
gary <beliefloatsky@gmail.com>
2021-07-16 20:21:38 +0800

    To-do-list 20210714 1.add 'add' function 2.add 'delete' function 3.add 'filter' function

--- src/App.js
+++ src/App.js
@@ -4,33 +4,107 @@ import Nav from './components/Nav.js'
 import AddItem from './components/AddItem.js'
 import ListOfButton from './components/ListOfButton.js'
 import ToDoList from './components/ToDoList.js'
+import {nanoid} from 'nanoid';
 
 class App extends React.Component {
-  // constructor(props){
-  //   super(props);
-  //   this.state={
-  //     singUp:["暱稱","帳號", "密碼"],
-  //     singIn:["帳號", "密碼"],
-  //     check:true
-  //   };
-  // }
-  //lifecycle
-  componentDidMount(){
-    console.log("Welcome~~")
+  constructor(props){
+    super(props);
+    this.state={
+      task:[{id:nanoid(), name:"跑步", completed:false},
+            {id:nanoid(), name:"洗浴室", completed:false},
+            {id:nanoid(), name:"讀書", completed:false}],
+      taskCompleted:[],
+      taskNotCompleted:[],
+      showState:"all",
+    };
+    this.changeItem = this.changeItem.bind(this);
+    this.findAndDelete = this.findAndDelete.bind(this);
+    this.changeCheckbox = this.changeCheckbox.bind(this);
+    this.showCompleted = this.showCompleted.bind(this);
+    this.showNotCompleted = this.showNotCompleted.bind(this);
+    this.showAll = this.showAll.bind(this)
   }
-  componentDidUpdate() {
-    console.log("Update")
+  //lifecycle
+  // componentDidMount(){
+  //   console.log("Welcome~~")
+  // }
+  // componentDidUpdate() {
+  //   console.log("Update")
+  // }
+
+  changeItem(newTask){ //新增to-do list item
+    var joined = this.state.task.concat(newTask);
+    this.setState({ task: joined })
+    //this.setState({ task: [...this.state.task, newTask] });
+    //this.setState({task:this.state.task});
+  }
+
+  findAndDelete(e){ //找到要刪除的object並且刪除
+    let targetIndex;
+    for(let i = 0; i < this.state.task.length; i++)
+    {
+      if(this.state.task[i].id == e.target.id)
+      {
+        targetIndex = i;
+      }
+    }
+    let array = [...this.state.task];
+    array.splice(targetIndex, 1);
+    this.setState({ task: array });
+    if(this.state.showState == "completed"){
+      array = array.filter(item => item.completed == true);
+      this.setState({taskCompleted : array});
+    }
+    else if(this.state.showState == "notCompleted"){
+      array = array.filter(item => item.completed == false);
+      this.setState({taskNotCompleted : array});
+    }
   }
   
+  changeCheckbox(e){ //改變completed為true或false
+    console.log(e.target.name);
+    let targetIndex;
+    for(let i = 0; i < this.state.task.length; i++) //找到目標index
+    {
+      if(this.state.task[i].id == e.target.name)
+      {
+        targetIndex = i;
+      }
+    }
+    let array = [...this.state.task]; //複製
+    array[targetIndex].completed = e.target.checked;
+    this.setState({ task: array });
+  }
+
+  showCompleted(){ //把原task陣列filter，由taskCompleted接收
+    let array = this.state.task.filter(item => item.completed == true);
+    this.setState({taskCompleted : array});
+    this.setState({showState:"completed"});
+  }
+
+  showNotCompleted(){ //把原task陣列filter，由taskNotCompleted接收
+    let array = this.state.task.filter(item => item.completed == false);
+    this.setState({taskNotCompleted : array});
+    this.setState({showState:"notCompleted"});
+  }
+
+  showAll(){
+    this.setState({showState:"all"});
+  }
+
   render(){
     return (
-      <div>
+      <div className="outline">
         <Nav />
-        {/* <SingIn name={this.state.singIn} onClick={()=>{this.setState({check:!this.state.check})}} />
-        <SingUp name={this.state.singUp} onClick={()=>{this.setState({check:!this.state.check})}} /> */}
-        <AddItem />
-        <ListOfButton />
-        <ToDoList name={["跑步", "洗浴室", "讀書"]} />
+        <AddItem name={this.state.task} changeItem={this.changeItem} />
+        <ListOfButton showCompleted={this.showCompleted} showNotCompleted={this.showNotCompleted} showAll={this.showAll}/>
+        {
+          this.state.showState == "all"?
+          <ToDoList name={this.state.task} findAndDelete={this.findAndDelete} changeCheckbox={this.changeCheckbox}/>
+          :this.state.showState == "completed"?
+            <ToDoList name={this.state.taskCompleted} findAndDelete={this.findAndDelete} changeCheckbox={this.changeCheckbox}/>
+            :<ToDoList name={this.state.taskNotCompleted} findAndDelete={this.findAndDelete} changeCheckbox={this.changeCheckbox}/>
+        } 
       </div>
     );
   }
--- src/components/ListOfButton.js
+++ src/components/ListOfButton.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 
 
-function List_of_button(props){
+function ListOfButton(props){
     return(
         <div>
-            <button>全部</button>
-            <button>已完成</button>
-            <button>未完成</button>
+            <button onClick={props.showAll} >全部</button>
+            <button onClick={props.showCompleted} >已完成</button>
+            <button onClick={props.showNotCompleted}>未完成</button>
         </div>
     )
 }
 
-export default List_of_button;
+export default ListOfButton;
